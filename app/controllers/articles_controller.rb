58e3796c61a418654c8449d2062ca151c08e7846
Profsain <profsaincrown@gmail.com>
2022-12-11 23:02:01 +0000

    Add crud operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,47 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to @article
+    else 
+      flash[:alert] = 'Article was not created'
+      render 'new'
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to @article
+    else
+      flash[:alert] = 'Article was not updated'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path
+  end
+
+  private 
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
