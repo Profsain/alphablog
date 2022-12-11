6e2b77fe8841a90524f3f6e7ac9c19a1a98a75bf
Profsain <profsaincrown@gmail.com>
2022-12-11 04:36:50 +0000

    Add articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  # Get all articles
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
