63b637c9c801aebe3ab85e81fe04a6589d67ca55
Profsain <profsaincrown@gmail.com>
2022-12-26 14:41:57 +0000

    remove whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true length: { minimum: 3, maximum: 50 }
+  validates :username, length: { minimum: 3, maximum: 100 }, presence: true
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: email_regex }, uniqueness: { case_sensitive: false }
-
 end
