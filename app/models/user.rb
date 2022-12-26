aef8049e1d7d883c1b29884c796279ffa67e8b65
Profsain <profsaincrown@gmail.com>
2022-12-26 13:53:03 +0000

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
-  validates :username, length: { minimum: 3, maximum: 100 }, presence: true
+
+  validates :username, presence: true, uniqueness: true length: { minimum: 3, maximum: 50 }
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: email_regex }, uniqueness: { case_sensitive: false }
- 
 
 end
