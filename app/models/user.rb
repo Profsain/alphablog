0235162516abd005849bda179b91d24fff3bf941
Profsain <profsaincrown@gmail.com>
2022-12-26 15:10:42 +0000

    Added before_save formating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :articles, dependent: :destroy
 
+  before_save { 
+    self.email = email.downcase
+    self.username = username.downcase
+  }
   validates :username, length: { minimum: 3, maximum: 100 }, presence: true
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: email_regex }, uniqueness: { case_sensitive: false }
