ead0eb575ec599bb24f5bc32177644f21f26b54c
Profsain <profsaincrown@gmail.com>
2023-01-04 03:02:36 +0000

    Added user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
+  # get all users
+  def index
+    @users = User.all
+  end
+
   def new
-    @user = User.new
+    @user = get_user
   end
 
   def create
-    @user = User.new(user_params)
+    @user = get_user
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha blog #{@user.username}! You have successfully signed up"
@@ -18,24 +23,32 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = get_user
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = get_user
 
     if @user.update(user_params)
       flash[:notice] = "Your profile was updated successfully"
-      redirect_to articles_path
+      redirect_to @user
     else
       render "edit"
     end
+  end
 
+  def show
+    @user = get_user
+    @articles = @user.articles
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def get_user
+    User.find(params[:id])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    # get the email from URL-parameters and make lowercase
+    email_address = user.email.downcase
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # extract size
+    size = options[:size]
+    # compile URL which can be used in image_tag
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+
+    # return an image_tag
+    image_tag(gravatar_url, alt: user.username, class: "rounded m-2 d-block img-thumbnail")
+  end
 end
